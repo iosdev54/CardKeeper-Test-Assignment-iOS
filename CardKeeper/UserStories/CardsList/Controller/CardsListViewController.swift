c50181663c170b5451bd930328eea4c233c190d7
iosdev54 <iosdev54@gmail.com>
2023-07-10 12:18:37 +0300

    Add MVC pattern to the app.

---
+++ CardKeeper/UserStories/CardsList/Controller/CardsListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CardsListViewController.swift
+//  CardKeeper
+//
+//  Created by Dmytro Grytsenko on 10.07.2023.
+//
+
+import UIKit
+
+class CardsListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationItem.title = "Картки"
+    }
+}
